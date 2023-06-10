41e300440b7c6db5e520a12ffb4d5d6678738c0f
Hamdhani Vaniza <redstonesandhamdhani@gmail.com>
2023-06-10 19:45:03 +0700

    Create main.py
    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import os
+import random
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+sampah_organik=["buah","daun"]
+sampah_anorganik=["plastik"]
+
+@bot.command()
+async def pilih(ctx,sampah):
+    if sampah in sampah_organik:
+        await ctx.send("masukkan ke dalam organik")
+    if sampah in sampah_anorganik:
+        await ctx.send("masukan ke dalam anorganik")
+bot.run("MTEwNjgwNDQ0MjEzNDc0NTE1OA.GEwcDg.aUlJV6DvOy-t69Pj1KFRm-sp_Y_NvUH7QEYkBk")
